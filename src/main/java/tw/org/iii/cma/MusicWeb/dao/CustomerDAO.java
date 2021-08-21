9cc65fd30b01ad8cd1ccd1e074b99e146dc61975
pursuelin <84397978+pursuelin@users.noreply.github.com>
2021-08-21 17:08:56 +0800

    Delete src directory

--- src/main/java/tw/org/iii/cma/MusicWeb/dao/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package tw.org.iii.cma.MusicWeb.dao;
-
-import tw.org.iii.cma.MusicWeb.domain.CustomerBean;
-
-public interface CustomerDAO {
-	public abstract CustomerBean select(String custid);
-	public abstract boolean update(byte[] password, String email,
-			java.util.Date birth, String custid);
-}
